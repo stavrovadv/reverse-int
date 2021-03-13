28674059c05d0affbdd4274206f64eb89b4c41f8
stavrovadv <stavrovadaria97@gmail.com>
2021-03-13 23:08:03 +0500

    feat: implement function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let nToString = Math.abs(n).toString();
+  let resArr = [];
+  for (let i = nToString.length - 1; i >= 0; i--) {
+    resArr.push(nToString.charAt(i));
+  }
+  return +resArr.join('');
 }
